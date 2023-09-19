c6c0e80772463ab086a5a1f68440330d91ef2839
Marco L <mfl-git@bitblox.de>
2023-09-19 21:46:15 +0200

    Renamed field `type` to `className` and used static string to avoid headaches after post-processing (minify & co.).

--- node-app/api/models/Model.ts
+++ node-app/api/models/Model.ts
@@ -4,7 +4,7 @@ export abstract class Model extends Object {
     updated: string;
     version: number = 1;
     hash: string = null;
-    type: string = typeof Model;
+    className: string = 'Model';
 
     constructor() {
         super();
