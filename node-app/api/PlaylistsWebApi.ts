3f8fa8f3904c9330accadde98f04ae7552b0d69c
Marco L <mfl-git@bitblox.de>
2023-09-24 21:56:57 +0200

    Added CRUD-methods. Still empty and need to implement.

--- node-app/api/PlaylistsWebApi.ts
+++ node-app/api/PlaylistsWebApi.ts
@@ -8,12 +8,32 @@ export class PlaylistsWebApi extends Object {
     // FIXME: Get that path from config. And decouple it somehow.
     static readonly REPO: PlaylistsRepository = new PlaylistsRepository(new SimpleJSONFileModelStorage(path.join(__dirname, '..', '..', 'data', 'playlists.json')));
     static readonly API_VERSION: string = 'v1';
-    static readonly API_BASEPATH: string = `/api/${PlaylistsWebApi.API_VERSION}`;
+    static readonly API_BASEPATH: string = `/api/${PlaylistsWebApi.API_VERSION}/playlists`;
 
     constructor(app: Application) {
         super();
         Object.setPrototypeOf(this, PlaylistsWebApi.prototype);
 
         // TODO: Register methods for handling CRUD on playlists
+
+        // Create
+        app.post(`${PlaylistsWebApi.API_BASEPATH}/`, (request: Request, response: Response) => {
+        });
+
+        // Read all
+        app.get(`${PlaylistsWebApi.API_BASEPATH}/`, (request: Request, response: Response) => {
+        });
+
+        // Read one
+        app.get(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
+        });
+
+        // Update one
+        app.patch(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
+        });
+
+        // Delete one
+        app.delete(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
+        });
     }
 };
