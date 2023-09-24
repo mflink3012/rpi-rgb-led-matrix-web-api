9ee5e67846fa738b21f6a4e71a672efc9c2013fc
Marco L <mfl-git@bitblox.de>
2023-09-24 21:47:19 +0200

    Renamed `RENDER_CONFIGS_REPO` to just `REPO` for consistency with similar repository-holders.

--- node-app/api/RenderConfigsWebApi.ts
+++ node-app/api/RenderConfigsWebApi.ts
@@ -7,7 +7,7 @@ import path from "path";
 
 export class RenderConfigsWebApi extends Object {
     // FIXME: Get that path from config. And decouple it somehow.
-    static readonly RENDER_CONFIGS_REPO: RenderConfigsRepository = new RenderConfigsRepository(new SimpleJSONFileModelStorage(path.join(__dirname, '..', '..', 'data', 'renderConfigs.json')));
+    static readonly REPO: RenderConfigsRepository = new RenderConfigsRepository(new SimpleJSONFileModelStorage(path.join(__dirname, '..', '..', 'data', 'renderConfigs.json')));
     static readonly API_VERSION: string = 'v1';
     static readonly API_BASEPATH: string = `/api/${RenderConfigsWebApi.API_VERSION}`;
 
@@ -22,7 +22,7 @@ export class RenderConfigsWebApi extends Object {
             let result: express.Response<any, Record<string, any>> = null;
 
             try {
-                result = response.status(201).send(RenderConfigsWebApi.RENDER_CONFIGS_REPO.create(renderConfig));
+                result = response.status(201).send(RenderConfigsWebApi.REPO.create(renderConfig));
             } catch (error) {
                 result = response.status(400).send(new ErrorObject('RENDER_CONFIG:CREATE_FAILED', error.toString()))
             }
@@ -31,19 +31,19 @@ export class RenderConfigsWebApi extends Object {
         });
 
         app.patch(`${RenderConfigsWebApi.API_BASEPATH}/renderconfigs/:id`, (request: Request, response: Response) => {
-            if (!RenderConfigsWebApi.RENDER_CONFIGS_REPO.contains(request.params.id)) {
+            if (!RenderConfigsWebApi.REPO.contains(request.params.id)) {
                 let error: ErrorObject = new ErrorObject('RENDER_CONFIG:NOT_FOUND', 'There is no render-config with id (id)!', { id: request.params.id });
                 return response.status(404).send(error);
             }
 
             let renderConfig: RenderConfig = request.body;
             renderConfig.id = request.params.id;
-            return response.status(200).send(RenderConfigsWebApi.RENDER_CONFIGS_REPO.update(renderConfig));
+            return response.status(200).send(RenderConfigsWebApi.REPO.update(renderConfig));
         });
 
         app.delete(`${RenderConfigsWebApi.API_BASEPATH}/renderconfigs/:id`, (request: Request, response: Response) => {
-            if (RenderConfigsWebApi.RENDER_CONFIGS_REPO.contains(request.params.id)) {
-                RenderConfigsWebApi.RENDER_CONFIGS_REPO.delete(request.params.id);
+            if (RenderConfigsWebApi.REPO.contains(request.params.id)) {
+                RenderConfigsWebApi.REPO.delete(request.params.id);
             } else if (!request.params.hasOwnProperty('missingError')) {
                 let error: ErrorObject = new ErrorObject('RENDER_CONFIG:NOT_FOUND', 'There is no render-config with id (id)!', { id: request.params.id });
                 return response.status(404).send(error);
@@ -53,16 +53,16 @@ export class RenderConfigsWebApi extends Object {
         });
 
         app.get(`${RenderConfigsWebApi.API_BASEPATH}/renderconfigs`, (request: Request, response: Response) => {
-            return response.status(200).send(RenderConfigsWebApi.RENDER_CONFIGS_REPO.readAll());
+            return response.status(200).send(RenderConfigsWebApi.REPO.readAll());
         });
 
         app.get(`${RenderConfigsWebApi.API_BASEPATH}/renderconfigs/:id`, (request: Request, response: Response) => {
-            if (!RenderConfigsWebApi.RENDER_CONFIGS_REPO.contains(request.params.id)) {
+            if (!RenderConfigsWebApi.REPO.contains(request.params.id)) {
                 let error: ErrorObject = new ErrorObject('RENDER_CONFIG:NOT_FOUND', 'There is no render-config with id (id)!', { id: request.params.id });
                 return response.status(404).send(error);
             }
 
-            return response.status(200).send(RenderConfigsWebApi.RENDER_CONFIGS_REPO.read(request.params.id));
+            return response.status(200).send(RenderConfigsWebApi.REPO.read(request.params.id));
         });
     }
 };
