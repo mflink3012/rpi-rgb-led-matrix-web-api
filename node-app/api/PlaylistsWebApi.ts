54ab0fa57c2e8c9cca0a98867a9127fd7e2cb50a
Marco L <mfl-git@bitblox.de>
2023-09-24 21:49:42 +0200

    Fixed naming of `PlaylistsWebApi`.

--- node-app/api/PlaylistsWebApi.ts
+++ node-app/api/PlaylistsWebApi.ts
@@ -4,15 +4,15 @@ import { PlaylistsRepository } from "./implementations/PlaylistsRepository";
 import { SimpleJSONFileModelStorage } from "./implementations/SimpleJSONFileModelStorage";
 import path from "path";
 
-export class RenderConfigsWebApi extends Object {
+export class PlaylistsWebApi extends Object {
     // FIXME: Get that path from config. And decouple it somehow.
     static readonly REPO: PlaylistsRepository = new PlaylistsRepository(new SimpleJSONFileModelStorage(path.join(__dirname, '..', '..', 'data', 'playlists.json')));
     static readonly API_VERSION: string = 'v1';
-    static readonly API_BASEPATH: string = `/api/${RenderConfigsWebApi.API_VERSION}`;
+    static readonly API_BASEPATH: string = `/api/${PlaylistsWebApi.API_VERSION}`;
 
     constructor(app: Application) {
         super();
-        Object.setPrototypeOf(this, RenderConfigsWebApi.prototype);
+        Object.setPrototypeOf(this, PlaylistsWebApi.prototype);
 
         // TODO: Register methods for handling CRUD on playlists
     }
