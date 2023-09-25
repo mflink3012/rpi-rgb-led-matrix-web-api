3ff0b6983038f0ecbaa8aa61b94e45df14ec1ecb
Marco L <mfl-git@bitblox.de>
2023-09-25 20:44:40 +0200

    Added create. Fixed deprecation of `res.send(status)`.

--- node-app/api/PlaylistsWebApi.ts
+++ node-app/api/PlaylistsWebApi.ts
@@ -3,6 +3,7 @@ import { ErrorObject } from "./ErrorObject";
 import { PlaylistsRepository } from "./implementations/PlaylistsRepository";
 import { SimpleJSONFileModelStorage } from "./implementations/SimpleJSONFileModelStorage";
 import path from "path";
+import { Playlist } from "./models/Playlist";
 
 export class PlaylistsWebApi extends Object {
     // FIXME: Get that path from config. And decouple it somehow.
@@ -18,27 +19,37 @@ export class PlaylistsWebApi extends Object {
 
         // Create
         app.post(`${PlaylistsWebApi.API_BASEPATH}/`, (request: Request, response: Response) => {
-            response.send(500).json(new ErrorObject('CREATE_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
+            let playlist: Playlist = request.body;
+
+            let result: express.Response<any, Record<string, any>> = null;
+
+            try {
+                result = response.status(201).send(PlaylistsWebApi.REPO.create(playlist));
+            } catch (error) {
+                result = response.status(400).send(new ErrorObject('PLAYLIST:CREATE_FAILED', error.toString()))
+            }
+
+            return result;
         });
 
         // Read all
         app.get(`${PlaylistsWebApi.API_BASEPATH}/`, (request: Request, response: Response) => {
-            response.send(500).json(new ErrorObject('READ_PLAYLISTS:NOT_IMPLEMENTED', 'Not yet implemented!'));
+            response.status(500).json(new ErrorObject('READ_PLAYLISTS:NOT_IMPLEMENTED', 'Not yet implemented!'));
         });
 
         // Read one
         app.get(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
-            response.send(500).json(new ErrorObject('READ_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
+            response.status(500).json(new ErrorObject('READ_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
         });
 
         // Update one
         app.patch(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
-            response.send(500).json(new ErrorObject('UPDATE_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
+            response.status(500).json(new ErrorObject('UPDATE_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
         });
 
         // Delete one
         app.delete(`${PlaylistsWebApi.API_BASEPATH}/:id`, (request: Request, response: Response) => {
-            response.send(500).json(new ErrorObject('DELETE_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
+            response.status(500).json(new ErrorObject('DELETE_PLAYLIST:NOT_IMPLEMENTED', 'Not yet implemented!'));
         });
     }
 };
