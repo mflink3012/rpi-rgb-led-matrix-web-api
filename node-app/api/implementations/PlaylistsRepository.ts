7d4781081b611f4301a99ef9e7f71f481efe65b0
Marco L <mfl-git@bitblox.de>
2023-09-25 20:36:27 +0200

    Fixed missing registry.

--- node-app/api/implementations/PlaylistsRepository.ts
+++ node-app/api/implementations/PlaylistsRepository.ts
@@ -1,10 +1,11 @@
 import { ModelRepository } from "../abstracts/ModelRepository";
 import { Playlist } from "../models/Playlist";
 import { ModelStorageInterface } from "../interfaces/StorageInterface";
+import { PlaylistRegistry } from "../models/PlaylistRegistry";
 
 export class PlaylistsRepository extends ModelRepository<Playlist> {
     constructor(storage :ModelStorageInterface<Playlist>) {
-        super(storage);
+        super(PlaylistRegistry, storage);
         Object.setPrototypeOf(this, PlaylistsRepository.prototype);
     }
 };
